9f056676c89fe6cc975dd4c852db2cf0ead244a8
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-10 18:57:24 +0600

    pratice operator code

---
+++ opert.js
@@ -0,0 +1,27 @@
+let x=30, y=23;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);
+console.log(x%y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
+
+console.log(x>=y?x:y);
+
+let g=2
+++g
+console.log(g)
+
+let h=9
+let a=h--
+console.log(a)
+console.log(h)
+let e=true, f=false;
+
+console.log(e && f)
+console.log(e || f)
+console.log(!e)
