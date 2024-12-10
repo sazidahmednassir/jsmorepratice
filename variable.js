bf83318eda6ac0fbb4f7881a3fc4984517aaa0c3
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-10 18:15:13 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,22 @@
+var a="sazid"
+
+console.log(a);
+
+var x=200, y=300;
+console.log(x, y)
+
+let s=500;
+
+console.log(s);
+
+
+console.log(t)
+var t=560;
+console.log(t)
+
+let i="Ahmed"
+console.log(i)
+
+const last="Nassir";
+console.log(last);
+console.log(typeof(last))
