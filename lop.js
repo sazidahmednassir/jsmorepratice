b411267a987e0954863f02e14e9e87f8bf15e274
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-24 18:25:36 +0600

    loop practice

---
+++ lop.js
@@ -0,0 +1,14 @@
+for (let i = 2; i <= 20; i++) {
+  console.log(`number is printing now: ${i}`);
+}
+
+var cv = [50, 30, 40, 50];
+for (let i = 0; i < cv.length; i++) {
+  console.log(`number is printing now: ${cv[i]}`);
+}
+
+let wx = ["red", "blue", "green", "black", 34, 12, 23];
+
+for (let x of wx) {
+  console.log(`Values are: "${x}"`);
+}
