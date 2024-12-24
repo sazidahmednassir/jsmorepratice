552e631701ebb1304e7a8081aa78d3d5af9f0ad8
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-24 17:58:19 +0600

    ternary operator

--- condition.js
+++ condition.js
@@ -1,34 +1,32 @@
-let c=33;
-let d=30;
+let c = 33;
+let d = 30;
 
-if(c<=d){
-    console.log("True")
-}
-else{
-    console.log("False")
-}
-
-
-let num=167;
-
-
-if(num%2==0){
-    console.log("Even")
-}
-else{
-    console.log("ODD")
-
+if (c <= d) {
+  console.log("True");
+} else {
+  console.log("False");
 }
 
+let num = 167;
 
-let e=30, f=230, g=23;
+if (num % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("ODD");
+}
 
-if(e>f && e>g){
-    console.log("e is the largest number "+e);
+let e = 30,
+  f = 230,
+  g = 23;
+
+if (e > f && e > g) {
+  console.log("e is the largest number " + e);
+} else if (f > e && f > g) {
+  console.log("f is the largest number " + f);
+} else {
+  console.log("g is the largest number " + g);
 }
-else if(f>e && f>g){
-    console.log("f is the largest number "+f);
-}
-else{
-    console.log("g is the largest number "+g);
-}
+
+e > f && e > g
+  ? console.log("e is the largest number " + e)
+  : console.log("g is the largest number " + g);
---
+++ swth.js
@@ -0,0 +1,16 @@
+let skill = "js";
+
+switch (skill) {
+  case "js":
+    console.log("JS");
+    break;
+  case "java":
+    console.log("JAVA");
+    break;
+  case "python":
+    console.log("PYTHON");
+    break;
+
+  default:
+    console.log("Pyhton is the best");
+}
